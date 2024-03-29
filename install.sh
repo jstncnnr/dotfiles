b8913b1b329d0795cb35db7bec03ad12c376ee76
Justin Conner <jstncnnr@outlook.com>
2024-03-29 14:07:28 -0700

    Add install.sh to setup symbolic links for config and etc files

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+declare -a dirs=(
+	"hypr" 
+	"kitty"
+	"nvim"
+	"swaync"
+	"waybar"
+	"wofi"
+)
+
+for i in "${dirs[@]}" 
+do
+	ln -sf "$(pwd)/config/$i" ~/.config/
+done
+
+declare -A etc=(
+	["pipewire--50-network.conf"]="/etc/pipewire/pipewire-pulse.conf.d/50-network.conf"
+	["pipewire--raop-discover.conf"]="/etc/pipewire/pipewire.conf.d/raop-discover.conf"
+)
+
+for key in "${!etc[@]}" 
+do
+	sudo ln -sf "$(pwd)/etc/$key" "${etc[$key]}"
+done
